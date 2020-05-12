5ab6640e0097b598624a19bd89f23b1240acbf60
Piotr Dobiech <pitcer@protonmail.com>
2020-05-12 22:43:35 +0200

    Add CompletionCodec factory method with Collection parameter

--- anfodis-command-completion/src/main/java/net/mcparkour/anfodis/command/codec/completion/CompletionCodec.java
+++ anfodis-command-completion/src/main/java/net/mcparkour/anfodis/command/codec/completion/CompletionCodec.java
@@ -1,14 +1,19 @@
 package net.mcparkour.anfodis.command.codec.completion;
 
+import java.util.Collection;
 import java.util.List;
 import net.mcparkour.anfodis.command.handler.CompletionContext;
 
 @FunctionalInterface
 public interface CompletionCodec {
 
-	List<String> getCompletions(CompletionContext context);
+	List<String> getCompletions(CompletionContext<?> context);
 
 	static CompletionCodec entries(String... completions) {
 		return context -> List.of(completions);
+	}
+
+	static CompletionCodec entries(Collection<String> completions) {
+		return context -> List.copyOf(completions);
 	}
 }
