520b7f3c288abadb34d78c7ed110272b40eb8672
Piotr Dobiech <pitcer@protonmail.com>
2020-06-03 15:43:15 +0200

    Update intext

--- anfodis-command/build.gradle.kts
+++ anfodis-command/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
 	api(project(":anfodis-core"))
-	api("net.mcparkour:intext-core:1.2.1")
+	api("net.mcparkour:intext-core:1.2.2")
 	api("net.mcparkour:craftmon-core:1.0.8")
 	implementation("net.mcparkour:common-text:1.0.7")
 }
