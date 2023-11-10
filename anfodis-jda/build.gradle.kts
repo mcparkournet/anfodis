44940e43e489e627728a937a9018caee43c91c02
Piotr Dobiech <pitcer@protonmail.com>
2023-11-10 14:32:09 +0100

    Update dependencies

--- anfodis-jda/build.gradle.kts
+++ anfodis-jda/build.gradle.kts
@@ -1,5 +1,13 @@
+repositories {
+    maven("https://m2.dv8tion.net/releases") {
+        content {
+            includeGroup("net.dv8tion")
+        }
+    }
+}
+
 dependencies {
     api(project(":anfodis-command"))
     api(project(":anfodis-listener"))
-    implementation("net.dv8tion:JDA:4.2.0_168")
+    implementation("net.dv8tion:JDA:4.4.0_350")
 }
